24d234252cd3343cdbbdca3bce455a3a8e0fcd74
Alex <alexsilvestri2000.work@gmail.com>
2026-05-28 16:30:28 +0200

    Email try number 3

--- src/main/java/com/example/doit/UserService.java
+++ src/main/java/com/example/doit/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 import java.util.Optional;
 
 @Service
@@ -12,6 +13,9 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     private final JavaMailSender mailSender;
+
+    @Value("${MAIL_USERNAME}")
+    private String fromEmail;
 
     public UserService(UserRepository userRepository, JavaMailSender mailSender) {
         this.userRepository = userRepository;
@@ -29,7 +33,7 @@ public class UserService {
     @Async
     public void sendVerificationEmailAsync(String to, String token) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("YOUR_EMAIL@gmail.com");
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject("Confirm Your Account");
         message.setText("Click here to verify: https://do-it-backend-6a5i.onrender.com/api/verify/" + token);
